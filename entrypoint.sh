f760a6e128f7287b2c98ae2a50a113af5469e605
nanda <nanda.bhikkhu@sasanarakkha.org>
2021-03-09 14:11:21 +0800

    better process handling & fritzbox restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,64 @@
-#!/bin/sh
+#!/bin/bash
 echo "entering $0"
-[ "${1}" = 'bash' ] && bash && exit 0
+[ "${1}" = 'bash' -o "$1" = "/bin/bash" ] && bash && exit 0
 
 
-REDIS_PIDFILE=/var/run/redis/redis-server.pid
-MYSQLD_PIDFILE=/var/run/mysqld/mysqld.pid
-#prepare mariadb
-gosu mysql /mariadb-entrypoint.sh prepare mysqld
+export REDIS_PIDFILE=/var/run/redis/redis-server.pid
+export MYSQLD_PIDFILE=/var/run/mysqld/mysqld.pid
+export FRITZ_PIDFILE=/var/run/fritzdump.pid
 
-# start services in background here
-/etc/init.d/redis-server start
-start-stop-daemon --start -b --quiet --oknodo --pidfile "${MYSQLD_PIDFILE}" --chuid mysql:mysql --exec /mariadb-entrypoint.sh -- mysqld
+function prepare_sql()
+{
+	#prepare mariadb
+	chown -R mysql:mysql "${MYSQL_ROOT}"
+	gosu mysql /mariadb-entrypoint.sh prepare mysqld
+}
 
-# run the main thing TODO: trap seems not to work :-(
-trap "echo TRAPed signal" HUP INT QUIT TERM
-/fritzdump/fritzdump.sh "$@"
+function start()
+{
+	# start services in background here
+	/etc/init.d/redis-server start 
+	start-stop-daemon --start -b --quiet --oknodo --pidfile "${MYSQLD_PIDFILE}" --chuid mysql:mysql --exec /mariadb-entrypoint.sh -- mysqld
+	for i in 1 1 1 2 5; do
+		echo 'show schemas;' |  mysql --user=$MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE &>/dev/null
+		[ "$?" = 0 ] && break;
+	done
 
-start-stop-daemon --stop --retry forever/TERM/1 --quiet --oknodo --pidfile ${MYSQLD_PIDFILE} --name mysqld
-start-stop-daemon --stop --retry forever/TERM/1 --quiet --oknodo --pidfile ${REDIS_PIDFILE} --name redis-server
+	# run the main thing
+	chown -R ntopng "${NTOPNG_DATA}"
+	/fritzdump/fritzdump.sh "$@" &
+	FRITZ_PID=$!
+	echo "$FRITZ_PID" > "${FRITZ_PIDFILE}"
+}
 
+function stopall()
+{
+	FRITZ_PID=$(cat "$FRITZ_PIDFILE")
+	if [ -n "${FRITZ_PID}" ]; then
+	#FRITZ_GPID=$(cat /proc/$(cat $FRITZ_PIDFILE)/stat | cut -d ' ' -f 5)
+		for i in 20 20; do
+			kill -s TERM -- $FRITZ_PID &>/dev/null
+			sleep 10
+			kill -0 -- $FRITZ_PID &>/dev/null || break
+			sleep $i
+		done
+
+		kill -9 -- $FRITZ_PID &>/dev/null
+		rm -rf "${FRITZ_PIDFILE}" &>/dev/null
+	fi
+
+	start-stop-daemon --stop --retry forever/TERM/1 --quiet --oknodo --pidfile ${MYSQLD_PIDFILE} --name mysqld
+	start-stop-daemon --stop --retry forever/TERM/1 --quiet --oknodo --pidfile ${REDIS_PIDFILE} --name redis-server
+}
+export -f stopall
+
+prepare_sql
+start
+
+# TODO: this still won't get propagated to ntopng
+trap "echo $0: TRAPed signal, terminating...; stopall;" HUP INT QUIT TERM
+
+wait
+stopall
 
 echo "exited $0"
--- fritzdump.sh
+++ fritzdump.sh
@@ -18,7 +18,8 @@ fi
 # Lan Interface
 IFACE="${IFACE:-1-lan}"
 NTOPNG_PORT="${NTOPNG_PORT:-8000}"
-NTOPNG_DATA="${NTOPNG_DATA:-./data}"
+NTOPNG_DATA="${NTOPNG_DATA:-$(dirname $BASH_SOURCE)/data/ntopng-$IFACE.data}"
+[ ! -d "${NTOPNG_DATA}" ] && mkdir -p "${NTOPNG_DATA}"
 
 EXTRA=""
 [ -n "${INSTANCE_NAME}" ] && EXTRA="${EXTRA} -N ${INSTANCE_NAME}"
@@ -32,33 +33,131 @@ else
     echo "no mysql db passed, no persistence"
 fi
 
-echo "Trying to login into $FRITZIP as user $FRITZUSER"
+CURL_PIDFILE="/var/run/curl.pid"
+NTOPNG_PIDFILE="/var/run/ntopng.pid"
 
-# Request challenge token from Fritz!Box
-CHALLENGE=$(curl -sk $FRITZIP/login_sid.lua |  grep -o "<Challenge>[a-z0-9]\{8\}" | cut -d'>' -f 2)
 
-# Very proprieatry way of AVM: Create a authentication token by hashing challenge token with password
-HASH=$(perl -MPOSIX -e '
-    use Digest::MD5 "md5_hex";
-    my $ch_Pw = "$ARGV[0]-$ARGV[1]";
-    $ch_Pw =~ s/(.)/$1 . chr(0)/eg;
-    my $md5 = lc(md5_hex($ch_Pw));
-    print $md5;
-  ' -- "$CHALLENGE" "$FRITZPWD")
+function run_capture()
+{
+	echo "Trying to login into $FRITZIP as user $FRITZUSER"
 
-SID="$(curl -sk "$FRITZIP/login_sid.lua" -d "response=$CHALLENGE-$HASH" -d 'username='${FRITZUSER} 2>/dev/null | grep -o "<SID>[a-z0-9]\{16\}" | cut -d'>' -f 2)"
 
-# Check for successfull authentification
-if [[ $SID =~ ^0+$ ]] ; then echo "Login failed. Did you create & use explicit Fritz!Box users?" ; exit 1 ; fi
+	# Request challenge token from Fritz!Box
+	CHALLENGE=$(curl -k -s $FRITZIP/login_sid.lua --connect-timeout 15 |  grep -o "<Challenge>[a-z0-9]\{8\}" | cut -d'>' -f 2)
+	[ -z "${CHALLENGE}" ] && echo "no challenge from fritzbox" >&2 && return 1
 
-echo "Got SID ${SID} ..."
-echo "Capturing traffic on Fritz!Box interface $IFACE ..." 1>&2
-echo "Using: $(ntopng --version)"
+	# Very proprieatry way of AVM: Create a authentication token by hashing challenge token with password
+	HASH=$(perl -MPOSIX -e '
+	    use Digest::MD5 "md5_hex";
+	    my $ch_Pw = "$ARGV[0]-$ARGV[1]";
+	    $ch_Pw =~ s/(.)/$1 . chr(0)/eg;
+	    my $md5 = lc(md5_hex($ch_Pw));
+	    print $md5;
+	  ' -- "$CHALLENGE" "$FRITZPWD")
+	  
+	SID=$(curl -k -s "$FRITZIP/login_sid.lua" --connect-timeout 15 -d "response=$CHALLENGE-$HASH" -d "username=${FRITZUSER}" 2>/dev/null | grep -o "<SID>[a-z0-9]\{16\}" | cut -d'>' -f 2)
 
-DATADIR="$(dirname $0)/data/ntopng-$IFACE.data"
-[ ! -d "${DATADIR}" ] && mkdir -p "${DATADIR}"
+	# Check for successfull authentification
+	if [[ "$SID" =~ ^0+$ ]] || [ -z "$SID" ] ; then echo "Login failed. Did you create & use explicit Fritz!Box users?" >&2 ; return 1; fi
 
-# TODO: trap seems not to work :-(
-trap "echo TRAPed signal" HUP INT QUIT TERM
-curl -sk -o - $FRITZIP/cgi-bin/capture_notimeout?ifaceorminor=$IFACE\&snaplen=\&capture=Start\&sid=$SID |\
-     ntopng -U ntopng -i - -n 1 -w "${NTOPNG_PORT}" -W 0 -n 1 -d "${NTOPNG_DATA}" ${EXTRA} --community
+	echo "Capturing traffic on Fritz!Box interface $IFACE ..." 1>&2
+	(/usr/bin/curl -sk -o - "$FRITZIP/cgi-bin/capture_notimeout?ifaceorminor=$IFACE&snaplen=&capture=Start&sid=$SID" | \
+		/usr/bin/ntopng -U ntopng -i - -n 1 -w "${NTOPNG_PORT}" -W 0 -n 1 -d "${NTOPNG_DATA}" ${EXTRA}) &
+
+	pgrep -lf /usr/bin/ntopng | cut -d ' ' -f 1 > "${NTOPNG_PIDFILE}"
+	pgrep -lf /usr/bin/curl | cut -d ' ' -f 1 > "${CURL_PIDFILE}"
+
+	kill -0 $(cat "${NTOPNG_PIDFILE}") || return 1
+	kill -0 $(cat "${CURL_PIDFILE}") || return 1
+}
+
+function stop_curl()
+{
+	echo "stopping curl" >&2
+	# nothing to do
+	[ ! -f "${CURL_PIDFILE}" ] && return 0
+	pid=$(cat "${CURL_PIDFILE}")
+
+	# try softly
+	for i in 1 1 1 2 5; do
+		kill -0 $pid &>/dev/null || break
+		kill -TERM $pid &>/dev/null
+		sleep $i
+	done
+
+	# force cleanup
+	kill -9 $pid &>/dev/null 
+	kill -0 "$pid" &>/dev/null || rm -f "${CURL_PIDFILE}" &>/dev/null
+}
+export -f stop_curl
+
+function stop_ntopng()
+{
+	echo "stopping ntopng" >&2
+
+	[ ! -f "${NTOPNG_PIDFILE}" ] && return 0
+	pid=$(cat "${NTOPNG_PIDFILE}")
+
+	# ntopng requires time to finish writing data, second TERM quits the writeback
+	for i in 20 20; do
+		kill -0 $pid &>/dev/null || break
+		kill -TERM $pid &>/dev/null
+		sleep 5
+		kill -0 $pid &>/dev/null || break
+		sleep $i
+	done
+
+	# force cleanup
+	kill -9 $pid &>/dev/null 
+	kill -0 "$pid" &>/dev/null || rm -f "${NTOPNG_PIDFILE}" &>/dev/null
+}
+export -f stop_ntopng
+
+function stop_capture()
+{
+	stop_curl
+	stop_ntopng
+}
+export -f stop_capture
+
+DIE_MARK=/var/run/die
+
+function watchdog()
+{
+	# check & restart reading
+	for i in 1 2 2 5 50 60 60 30 30 30 30; do
+		[ -f "${DIE_MARK}" ] && return 0
+		[ -f "${CURL_PIDFILE}" ] && kill -0 $(cat "${CURL_PIDFILE}") &>/dev/null && \
+			[ -f "${NTOPNG_PIDFILE}" ] && kill -0 $(cat "${NTOPNG_PIDFILE}") &>/dev/null && break
+
+		stop_capture
+		run_capture || stop_capture
+
+		sleep $i
+	done
+}
+
+function stopall()
+{
+	touch "${DIE_MARK}"
+
+	# stop capture
+	stop_capture
+
+	# remove pipe
+	[ -p "${PIPE}" ] && rm "${PIPE}" 
+}
+export -f stopall
+
+trap "echo $0: signal trapped, exiting...; stopall;" TERM INT HUP QUIT
+
+rm "${DIE_MARK}" &>/dev/null
+run_capture || stop_capture
+
+while [ ! -f "${DIE_MARK}" ]; do
+	sleep 5
+	watchdog
+done
+
+stopall
+
